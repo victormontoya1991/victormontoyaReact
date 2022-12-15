89d9f4ed0c72d2d32f0e2e72a2d8db59d184f4fb
victormontoya1991 <disvictormontoya@gmail.com>
2022-12-15 12:43:31 -0500

    cambio frod

--- react-victormontoyareact/src/App.js
+++ react-victormontoyareact/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lo logre perros 
         </a>
       </header>
     </div>
