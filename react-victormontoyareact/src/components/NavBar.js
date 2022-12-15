35ea613a3ca033f803f799bdb35a13b703163113
victormontoya1991 <disvictormontoya@gmail.com>
2022-12-15 16:06:45 -0500

    creacionprimercomponente

---
+++ react-victormontoyareact/src/components/NavBar.js
@@ -0,0 +1,13 @@
+
+
+
+export const NavBar = () => {
+
+    return(
+        <div>
+            <h2> Hola Mundo</h2>
+            <p>Este es un texto generico</p>
+            <hr/>
+        </div>
+    )
+}
