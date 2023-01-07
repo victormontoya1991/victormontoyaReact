57e7981da6118cb5314d6500ccfbbe0eb361231b
victormontoya1991 <disvictormontoya@gmail.com>
2023-01-06 20:04:45 -0500

    icon

--- react-victormontoyareact/src/components/UI/atoms/ContainerItenProduct/ContainerItenProduct.js
+++ react-victormontoyareact/src/components/UI/atoms/ContainerItenProduct/ContainerItenProduct.js
@@ -4,6 +4,9 @@ const ContainerItenProduct = ( {name, image, category, price, id }) => {
     return(
         <div className='ContainerItenProduct' id={id}>
             <div className='ImgItenProduct'>
+                <div className='IconFavorite'>
+                    <span className="material-icons md-70">favorite</span>
+                </div>
                 <img alt="{name}" src={image} />
             </div>
             <h4 className='TitleItenProduct'>{name}</h4>
