4397a96748bc5194a01b5989b487888a7aeb98d1
victormontoya1991 <disvictormontoya@gmail.com>
2023-01-17 10:12:49 -0500

    load contact

--- react-victormontoyareact/src/App.js
+++ react-victormontoyareact/src/App.js
@@ -1,7 +1,7 @@
 import ItemListContainer from './components/page/category/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/page/product/ItemDetailContainer/ItemDetailContainer';
 import { NavBar} from './components/UI/organisms/NavBar/NavBar';
-import { PageContact } from './components/page/contact/contact';
+import  PageContact from './components/page/contact/contact';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import'./Apps.scss'
 import { Foooter } from './components/UI/organisms/Footer/Footer';
--- react-victormontoyareact/src/components/page/contact/contact.js
+++ react-victormontoyareact/src/components/page/contact/contact.js
@@ -1,8 +1,51 @@
+import { useState } from 'react'
 import './contact.scss'
 
-export const PageContact = () => {
+const PageContact = () => {
+    const [values, setValues] = useState({
+        firstname :'',
+        email:'',
+        phone:''
+    })
+    const handleInputChage = (e) =>{
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Submit", values)
+    }
     return(
         <div className="MainContact">
+            <form className="contact" onSubmit={handleSubmit} >
+                <input 
+                    onChange={handleInputChage}
+                    value={values.firstname}
+                    name='firstname'
+                    type="text" 
+                    placeholder="Nombre"
+                    className='ImputClass'
+                    /> <hr/>
+                <input 
+                    onChange={handleInputChage}
+                    value={values.email}
+                    name='email'
+                    type="email" 
+                    placeholder="Email"
+                    /> <hr/>
+                <input 
+                    onChange={handleInputChage}
+                    value={values.phone}
+                    name='phone'
+                    type="phone" 
+                    placeholder="Telefono"
+                    /> <hr/>
+                <button>Enviar</button>
+            </form>
         </div>
     )
 }
+
+export default PageContact
