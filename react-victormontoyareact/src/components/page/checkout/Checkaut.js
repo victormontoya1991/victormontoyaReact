39e13a4f760a237665523004cfa34a3dc0f11fc5
victormontoya1991 <disvictormontoya@gmail.com>
2023-02-21 08:40:11 -0500

    upade formulario

--- react-victormontoyareact/src/components/page/checkout/Checkaut.js
+++ react-victormontoyareact/src/components/page/checkout/Checkaut.js
@@ -5,7 +5,6 @@ import { useCarContext } from '../../../context/CarContext'
 import { db } from '../../../firebase/config'
 import { collection, addDoc, documentId, writeBatch, where, query, getDocs } from 'firebase/firestore'
 
-
 const User = () => {
     const {mycar, totalCar, formatterPeso, discountCar, emptyCar} = useCarContext()
     const [ordenId, setOrdenId] = useState(null)
@@ -24,56 +23,63 @@ const User = () => {
     }
     const hadleSubmit = async (e) =>{
         e.preventDefault()
+        let isFormValid = true;
         //Validacion datos
         if(values.name.length < 3){
-            alert("Nombre invalido")
+            alert("Nombre inválido")
+            isFormValid = false;
         }
         if (values.andress.length < 10) {
             alert('Dirección inválida. Debe tener al menos 10 caracteres.');
+            isFormValid = false;
         }
         if (!/^\d{10}$/.test(values.phone)){
             alert('Teléfono inválido. Debe tener 10 dígitos numéricos.');
+            isFormValid = false;
         }
         // eslint-disable-next-line no-useless-escape
         if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(values.email)){
             alert('Correo electrónico inválido.');
+            isFormValid = false;
         }
-        //Modelado de la orden
-        const order = {
-            coustumer: values,
-            items: mycar,
-            total: formatterPeso.format(totalCar()-discountCar())
-        }
-        // Llamado de stock en base de datos y data reposa la orden
-        const batch = writeBatch(db)
-        const ordenRef = collection(db,'data_orders')
-        const producRef = collection (db, 'data_produc')
-        // Construccion de array sin inventario o stock
-        const outOfStock = []
-        const itemsRef= query(producRef, where( documentId(),'in', mycar.map(prod => prod.id))) 
-        const productsCar = await getDocs(itemsRef)
-        productsCar.docs.forEach(doc => {
-            const item = mycar.find(item=> item.id === doc.id)
-            if(doc.data().stock >= item.counter){
-                batch.update(doc.ref, {
-                    stock: doc.data().stock - item.counter
-                })
-            } else {
-                outOfStock.push(item)
+        if (isFormValid) {
+            //Modelado de la orden
+            const order = {
+                coustumer: values,
+                items: mycar,
+                total: formatterPeso.format(totalCar()-discountCar())
             }
-        })
-        if(outOfStock.length === 0){
-            batch.commit()
-                .then(()=>{
-                    addDoc(ordenRef, order)
-                        .then((doc)=> {
-                            setOrdenId(doc.id)
-                            emptyCar()
-                        }) 
-                        .catch((err)=> console.log(err))
-                })
-        } else {
-            alert ("Upss algunos productos no tenemos el stock suficiente para la orden")
+            // Llamado de stock en base de datos y data reposa la orden
+            const batch = writeBatch(db)
+            const ordenRef = collection(db,'data_orders')
+            const producRef = collection (db, 'data_produc')
+            // Construccion de array sin inventario o stock
+            const outOfStock = []
+            const itemsRef= query(producRef, where( documentId(),'in', mycar.map(prod => prod.id))) 
+            const productsCar = await getDocs(itemsRef)
+            productsCar.docs.forEach(doc => {
+                const item = mycar.find(item=> item.id === doc.id)
+                if(doc.data().stock >= item.counter){
+                    batch.update(doc.ref, {
+                        stock: doc.data().stock - item.counter
+                    })
+                } else {
+                    outOfStock.push(item)
+                }
+            })
+            if(outOfStock.length === 0){
+                batch.commit()
+                    .then(()=>{
+                        addDoc(ordenRef, order)
+                            .then((doc)=> {
+                                setOrdenId(doc.id)
+                                emptyCar()
+                            }) 
+                            .catch((err)=> console.log(err))
+                    })
+            } else {
+                alert ("Upss algunos productos no tenemos el stock suficiente para la orden")
+            }
         }
     }
     if(ordenId){
@@ -91,7 +97,7 @@ const User = () => {
     return (
         <div className='ContainerCheckaut'>
             <h2>Finalizar Compra</h2>
-            <form onSubmit={hadleSubmit}>
+            <form onSubmit={hadleSubmit} className='DatesCheckaut'>
                 <input
                     onChange={handleInputChange}
                     type='text'
@@ -120,7 +126,7 @@ const User = () => {
                     value={values.email}
                     placeholder='Email'
                 />
-                <button>Enviar</button>
+                <button>Pago Contra Entrega</button>
             </form>
         </div>
     );
